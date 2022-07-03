a81a52f9f24dc9715dab6fe651fc3806e6fe0b8c
unknown <zzharuk@gmail.com>
2022-07-03 14:08:45 +0300

    username changed

--- deploy-github-pages.sh
+++ deploy-github-pages.sh
@@ -11,6 +11,6 @@ git init
 git add .
 git commit -m 'deploy'
 
-git push -f https://github.com/zzharuk/currency-exchange-calculator.git master:gh-pages
+git push -f https://github.com/zharuk-alex/currency-exchange-calculator.git master:gh-pages
 
 cd -
